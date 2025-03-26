56eb7a0fca0ff0f61e23c30d248363fa5afff5f0
VamsiMannaru <vamsi.mannaru@navayuvabharatinfotech.com>
2025-03-26 16:54:53 +0530

    Updated Genericfunctions.sql

---
+++ SQL/Genericfunctions.sql
@@ -0,0 +1,64 @@
+SELECT
+    *
+FROM
+    employees;
+
+SELECT
+    e.employee_id,
+    e.first_name,
+    e.last_name,
+    e.phone_number,
+    e.manager_id,
+    nvl(e.manager_id, 'No Manager') nvl_manager_id
+FROM
+    employees e;
+
+desc employees;
+
+SELECT
+    e.employee_id,
+    e.first_name,
+    e.last_name,
+    e.phone_number,
+    e.manager_id,
+    nvl(to_char(e.manager_id),
+        'No Manager')    nvl_manager_id_char,
+    nvl(e.manager_id, 0) nvl_manager_id_number
+FROM
+    employees e;
+
+SELECT
+    e.employee_id,
+    e.first_name,
+    e.last_name,
+    e.phone_number,
+    e.manager_id,
+    e.commission_pct,
+    nvl(e.commission_pct, 0) commission_pct_nvl
+FROM
+    employees e;
+
+SELECT
+    e.employee_id,
+    e.first_name,
+    e.last_name,
+    e.phone_number,
+    e.manager_id,
+    e.commission_pct,
+    nvl(e.commission_pct, 0)                                                
+    nvl2(e.commission_pct,
+         round(commission_pct, 1),
+         0)                                                                 
+    nvl2(e.commission_pct, 'Commission Percentage Exists', 'No Commission') 
+FROM
+    employees e;
+
+desc employees;
+    
+--GREATEST & LEAST
+
+SELECT
+    greatest(1, 2, 3, 6, 8),
+    least(1, 2, 3, 6, 8)
+FROM
+    dual;
